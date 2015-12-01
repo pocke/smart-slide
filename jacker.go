a3647cb43dbf02b5f32ba3221887c4a2eb7489b6
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-01 18:46:18 +0900

    Split file

---
+++ jacker.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type WriteJacker struct {
+	buf    *bytes.Buffer
+	header http.Header
+	status int
+}
+
+func NewWriteJacker() *WriteJacker {
+	return &WriteJacker{
+		buf:    bytes.NewBuffer([]byte{}),
+		header: make(map[string][]string),
+		status: 200,
+	}
+}
+
+func (wj *WriteJacker) Header() http.Header {
+	return wj.header
+}
+
+func (wj *WriteJacker) WriteHeader(i int) {
+	wj.status = i
+}
+
+func (wj *WriteJacker) Write(b []byte) (int, error) {
+	return wj.buf.Write(b)
+}
+
+func (wj *WriteJacker) InjectScript(w http.ResponseWriter) {
+	var body []byte
+	if !strings.Contains(wj.Header().Get("Content-Type"), "text/html") {
+		body = wj.buf.Bytes()
+	} else {
+		s := strings.Replace(wj.buf.String(), "</body>", "<script>alert('hoge')</script></body>", 1)
+		body = []byte(s)
+		wj.header.Set("Content-Length", strconv.Itoa(len(body)))
+	}
+
+	for key, vals := range wj.header {
+		for _, v := range vals {
+			w.Header().Add(key, v)
+		}
+	}
+	w.Write(body)
+	w.WriteHeader(wj.status)
+}
+
+var _ http.ResponseWriter = &WriteJacker{}
