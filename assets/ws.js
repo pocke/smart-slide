3cd303a849bfaa6386bc6fd4a1fe44aa005eb550
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-03 22:52:43 +0900

    websocket to location.hostname

--- assets/ws.js
+++ assets/ws.js
@@ -1,5 +1,5 @@
 // TODO: localhost
-var conn = new WebSocket("ws://localhost:" + port.toString());
+var conn = new WebSocket("ws://" + location.hostname + ":" + port.toString());
 conn.onmessage = function () {
   slideshow.gotoNextSlide();
 };
