34a9edddc1dbd26c0b5fbbd56babbc6055605e74
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-04 13:28:36 +0900

    Can back slide

--- assets/ws.js
+++ assets/ws.js
@@ -1,5 +1,10 @@
 // TODO: localhost
 var conn = new WebSocket("ws://" + location.hostname + ":" + port.toString());
-conn.onmessage = function () {
-  slideshow.gotoNextSlide();
+conn.onmessage = function (ev) {
+  var text = JSON.parse(ev.data);
+  if (text === 'right') {
+    slideshow.gotoNextSlide();
+  } else {
+    slideshow.gotoPreviousSlide();
+  }
 };
